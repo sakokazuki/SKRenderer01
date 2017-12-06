57a4cdfd9afcacc8fc497453f1d8a6afaa2b12ee
sakokazuki <sako@aid-dcc.com>
2017-12-06 20:48:50 +0900

    Initial Commit

---
+++ Game01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game01
+//
+//  Created by kazuki sako on 2017/12/06.
+//  Copyright © 2017年 kazuki sako. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
