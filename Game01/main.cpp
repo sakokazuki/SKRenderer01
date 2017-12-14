1facfff4923e858cd2366693276fc978e1b28113
sakokazuki <sako@aid-dcc.com>
2017-12-14 10:26:37 +0900

    12/13

--- Game01/main.cpp
+++ Game01/main.cpp
@@ -6,22 +6,19 @@
 //  Copyright © 2017年 kazuki sako. All rights reserved.
 //
 
-#include <cstdio>
 #include <cstdlib>
-
-#include <iostream>
 #include <string>
-
+#include <iostream>
 #include <fstream>
-#include <sstream>
-
-
+using std::ifstream;
+#include <vector>
+#include <algorithm>
+#include <GL/glew.h>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdocumentation"
 #include <GLFW/glfw3.h>
 #pragma clang diagnostic pop
-#include <OpenGL/gl3.h>
 
 const int G_WIDTH = 640;
 const int G_HEIGHT = 480;
@@ -63,6 +60,7 @@ bool CreateCompileShader( std::string code, GLenum shaderType, GLuint& shader )
     
     // Load the source code into the shader object
     const GLchar* codeArray[] = { code.c_str() };
+    std::cout << "----- old -----" << code.c_str() << "----------" << std::endl;
     glShaderSource( shader, 1, codeArray, NULL );
     
     // Compile the shader
@@ -143,9 +141,255 @@ bool LinkShader( GLint shader1, GLint shader2 )
     return true;
 }
 
+bool CreateCompileShaderFromFile(){
+    return false;
+}
+
+GLint test(){
+    // Create the shader object
+    GLuint fragShader = glCreateShader( GL_FRAGMENT_SHADER );
+    if( 0 == fragShader )
+    {
+        fprintf(stderr, "Error creating vertex shader.\n");
+        exit(1);
+    }
+    const GLchar* codeArray[] = {shader_basic_frag.c_str()};
+    glShaderSource( fragShader, 1, codeArray, NULL );
+    
+    // Compile the shader
+    glCompileShader( fragShader );
+    
+    // Check compilation status
+    GLint result;
+    glGetShaderiv( fragShader, GL_COMPILE_STATUS, &result );
+    if( GL_FALSE == result )
+    {
+        
+        fprintf( stderr, "Vertex shader compilation failed!\n" );
+        
+        GLint logLen;
+        glGetShaderiv( fragShader, GL_INFO_LOG_LENGTH, &logLen );
+        
+        if( logLen > 0 )
+        {
+            char * log = (char *)malloc(logLen);
+            
+            GLsizei written;
+            glGetShaderInfoLog(fragShader, logLen, &written, log);
+            
+            fprintf(stderr, "Shader log: \n%s", log);
+            
+            free(log);
+        }
+    }
+    return result;
+}
+
+GLint bothShader(std::string filename, GLenum shaderType){
+    GLchar * shaderCode;
+    
+    // Load contents of file into shaderCode here…
+    ifstream inFile( "/Users/kazukisako/Documents/practice/game/Game01/Game01/shaders/"+filename, ifstream::in );
+    if( !inFile ) {
+        fprintf(stderr, "Error opening file: shaders/point.vert\n" );
+        exit(1);
+    }
+    
+    shaderCode = (char *)malloc(10000);
+    int i = 0;
+    while( inFile.good() ) {
+        int c = inFile.get();
+        
+        if(c == -1){
+            break;
+        }
+        shaderCode[i++] = c;
+    }
+    inFile.close();
+    shaderCode[i++] = '\0';
+    
+    // Create the shader object
+    GLuint vertShader = glCreateShader( shaderType );
+    if( 0 == vertShader )
+    {
+        fprintf(stderr, "Error creating vertex shader.\n");
+        exit(1);
+    }
+    
+    // Load the source code into the shader object
+    const GLchar* codeArray[] = {shaderCode};
+    glShaderSource( vertShader, 1, codeArray, NULL );
+    free(shaderCode); // can be removed from book.
+    
+    // Compile the shader
+    glCompileShader( vertShader );
+    
+    // Check compilation status
+    GLint result;
+    glGetShaderiv( vertShader, GL_COMPILE_STATUS, &result );
+    if( GL_FALSE == result )
+    {
+        
+        fprintf( stderr, "Vertex shader compilation failed!\n" );
+        
+        GLint logLen;
+        glGetShaderiv( vertShader, GL_INFO_LOG_LENGTH, &logLen );
+        
+        if( logLen > 0 )
+        {
+            char * log = (char *)malloc(logLen);
+            
+            GLsizei written;
+            glGetShaderInfoLog(vertShader, logLen, &written, log);
+            
+            fprintf(stderr, "Shader log: \n%s", log);
+            
+            free(log);
+        }
+    }
+    return result;
+}
+
+
+
+GLint fragshader(){
+    GLchar * shaderCode;
+    
+    // Load contents of file into shaderCode here…
+    ifstream fragFile( "/Users/kazukisako/Documents/practice/game/Game01/Game01/shaders/point.frag", ifstream::in );
+    if( !fragFile ) {
+        fprintf(stderr, "Error opening file: shader/basic.frag\n" );
+        exit(1);
+    }
+    
+    shaderCode = (char *)malloc(10000);
+    int i = 0;
+    while( fragFile.good() ) {
+        int c = fragFile.get();
+        if(c == -1){
+            break;
+        }
+        shaderCode[i++] = c;
+    }
+    fragFile.close();
+    shaderCode[i++] = '\0';
+    std::cout << shaderCode << std::endl;
+    ////////////////////////////////////////////
+    
+    // Create the shader object
+    GLuint fragShader = glCreateShader( GL_FRAGMENT_SHADER );
+    if( 0 == fragShader )
+    {
+        fprintf(stderr, "Error creating fragment shader.\n");
+        exit(1);
+    }
+    
+    // Load the source code into the shader object
+    
+    const GLchar* codeArray[] = {shaderCode};
+    glShaderSource( fragShader, 1, codeArray, NULL );
+    free(shaderCode); // can be removed from book.
+    
+    // Compile the shader
+    glCompileShader( fragShader );
+    
+    // Check compilation status
+    GLint result;
+    glGetShaderiv( fragShader, GL_COMPILE_STATUS, &result );
+    if( GL_FALSE == result )
+    {
+        
+        fprintf( stderr, "Fragment shader compilation failed!\n" );
+        
+        GLint logLen;
+        glGetShaderiv( fragShader, GL_INFO_LOG_LENGTH, &logLen );
+        
+        if( logLen > 0 )
+        {
+            char * log = (char *)malloc(logLen);
+            
+            GLsizei written;
+            glGetShaderInfoLog(fragShader, logLen, &written, log);
+            
+            fprintf(stderr, "Shader log: \n%s", log);
+            
+            free(log);
+        }
+    }
+    return result;
+}
+
+GLint vertshader(){
+    GLchar * shaderCode;
+    
+    // Load contents of file into shaderCode here…
+    ifstream inFile( "/Users/kazukisako/Documents/practice/game/Game01/Game01/shaders/point.vert", ifstream::in );
+    if( !inFile ) {
+        fprintf(stderr, "Error opening file: shaders/point.vert\n" );
+        exit(1);
+    }
+    
+    shaderCode = (char *)malloc(10000);
+    int i = 0;
+    while( inFile.good() ) {
+        int c = inFile.get();
+        
+        if(c == -1){
+            break;
+        }
+        shaderCode[i++] = c;
+    }
+    inFile.close();
+    shaderCode[i++] = '\0';
+    
+    // Create the shader object
+    GLuint vertShader = glCreateShader( GL_VERTEX_SHADER );
+    if( 0 == vertShader )
+    {
+        fprintf(stderr, "Error creating vertex shader.\n");
+        exit(1);
+    }
+    
+    // Load the source code into the shader object
+    const GLchar* codeArray[] = {shaderCode};
+    glShaderSource( vertShader, 1, codeArray, NULL );
+    free(shaderCode); // can be removed from book.
+    
+    // Compile the shader
+    glCompileShader( vertShader );
+    
+    // Check compilation status
+    GLint result;
+    glGetShaderiv( vertShader, GL_COMPILE_STATUS, &result );
+    if( GL_FALSE == result )
+    {
+        
+        fprintf( stderr, "Vertex shader compilation failed!\n" );
+        
+        GLint logLen;
+        glGetShaderiv( vertShader, GL_INFO_LOG_LENGTH, &logLen );
+        
+        if( logLen > 0 )
+        {
+            char * log = (char *)malloc(logLen);
+            
+            GLsizei written;
+            glGetShaderInfoLog(vertShader, logLen, &written, log);
+            
+            fprintf(stderr, "Shader log: \n%s", log);
+            
+            free(log);
+        }
+    }
+    return result;
+}
+
+
 int main(void)
 {
     GLFWwindow* window;
+    
+    atexit(glfwTerminate);
 
     /* Initialize the library */
     if (!glfwInit())
@@ -155,6 +399,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
     
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(G_WIDTH, G_HEIGHT, "GAME WINDOW", NULL, NULL);
@@ -167,17 +412,30 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     
+    // GLEW を初期化する
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        // GLEW の初期化に失敗した
+        std::cerr << "Can't initialize GLEW" << std::endl;
+        return 1;
+    }
+    
     glfwSwapInterval(1);
     
-//    // Create a Shader and Compile a Shader
+    GLuint vertShader = bothShader("point.vert", GL_VERTEX_SHADER);
+//    GLuint fragShader = fragshader();
+    // Create a Shader and Compile a Shader
     /////////////////// Create the VBO ////////////////////
-    GLuint vertShader, fragShader;
-    if(!CreateCompileShader(shader_basic_vert, GL_VERTEX_SHADER, vertShader)){
-        return -1;
-    }
-    if(!CreateCompileShader(shader_basic_frag, GL_FRAGMENT_SHADER, fragShader)){
-        return -1;
-    }
+    GLuint fragShader = bothShader("point.frag", GL_FRAGMENT_SHADER);
+    fragShader = test();
+////    vertShader = vertshader();
+//    if(!CreateCompileShader(shader_basic_vert, GL_VERTEX_SHADER, vertShader)){
+//        return -1;
+//    }
+//    if(!CreateCompileShader(shader_basic_frag, GL_FRAGMENT_SHADER, fragShader)){
+//        return -1;
+//    }
 
     if(!LinkShader(vertShader, fragShader)){
         return -1;
@@ -225,7 +483,7 @@ int main(void)
         /* Render here */
         glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
+        
         glBindVertexArray( vaoHandle );
         glDrawArrays( GL_TRIANGLES, 0, 3 );
         glBindVertexArray( 0 );
@@ -237,8 +495,7 @@ int main(void)
         glfwPollEvents();
     }
 
-    glfwTerminate();
-    return 0;
+
 }
 
 
