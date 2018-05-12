a7a1f6402de4eaeb281c188a9d6b9fd274c09b2f
sakokazuki <sako@aid-dcc.com>
2018-05-13 03:06:52 +0900

    tmp

---
+++ Game01/lights/SpotLight.cpp
@@ -0,0 +1,9 @@
+//
+//  SpotLight.cpp
+//  Game01
+//
+//  Created by kazuki sako on 2018/05/13.
+//  Copyright © 2018年 kazuki sako. All rights reserved.
+//
+
+#include "SpotLight.hpp"
---
+++ Game01/lights/SpotLight.hpp
@@ -0,0 +1,14 @@
+//
+//  SpotLight.hpp
+//  Game01
+//
+//  Created by kazuki sako on 2018/05/13.
+//  Copyright © 2018年 kazuki sako. All rights reserved.
+//
+
+#ifndef SpotLight_hpp
+#define SpotLight_hpp
+
+#include <stdio.h>
+
+#endif /* SpotLight_hpp */
